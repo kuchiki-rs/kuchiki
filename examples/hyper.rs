7398a604d8622c2a2612e69a732fdcc3ecc18967
Simon Sapin <simon.sapin@exyr.org>
2016-02-04 15:31:01 +0100

    Fix the hyper example.

--- examples/hyper.rs
+++ examples/hyper.rs
@@ -11,22 +11,9 @@ fn main() {
 fn main() {
     use kuchiki::traits::*;
 
-    let url = "https://www.mozilla.org/en-US/";
-    println!("{} - {} ", "Calling site ", url);
-
-    // Fetch and parse the html page
-    if let Ok(doc) = kuchiki::parse_html().from_http(url) {
-        println!("{}", "Finding Easter egg");
-
-        // Manually navigate to hidden comment
-        let x = doc.children().nth(1).unwrap()
-                    .first_child().unwrap()
-                    .children().nth(3).unwrap();
-
-        // Convert comment into RefString and borrow it
-        let comment = x.as_comment().unwrap().borrow();
-
-        println!("{}", *comment);
+    if let Ok(doc) = kuchiki::parse_html().from_http("https://www.mozilla.org/en-US/") {
+        let comment = doc.descendants().comments().next().unwrap();
+        println!("{}", *comment.borrow());
     } else {
         println!("{}", "The page couldn't be fetched");
     }
